1130e89858a7ddae77a0f3e74d6f1a60a75f89ce
Robert Beall <rbeall@quotient-inc.com>
2022-05-23 15:38:25 -0400

    updates with new search fields

--- src/Controller/FBSearchResultsController.php
+++ src/Controller/FBSearchResultsController.php
@@ -32,17 +32,48 @@ class FBSearchResultsController extends ControllerBase {
    */
   public function content($q, $place) {
     $q = str_replace(" ", "+", urldecode($q));
-    $results = $this->edanRequestManager->getNmaahcFBList($q, $place);
+
+    $params = [];
+
+    if(\Drupal::request()->query->has('fname'))
+    {
+      $params['fname'] = \Drupal::request()->query->get('fname');
+    }
+
+    if(\Drupal::request()->query->has('lname'))
+    {
+      $params['lname'] = \Drupal::request()->query->get('lname');
+    }
+
+    if(\Drupal::request()->query->has('location'))
+    {
+      $params['location'] = \Drupal::request()->query->get('location');
+    }
+
+    if(\Drupal::request()->query->has('rtype'))
+    {
+      $params['rtype'] = \Drupal::request()->query->get('rtype');
+    }
+
+    if(\Drupal::request()->query->has('date'))
+    {
+      $params['date'] = explode("|", \Drupal::request()->query->get('date'));
+    }
+
 
     $fb_config = \Drupal::config('fb_search.settings');
     $rows = $fb_config->get('display.rows');
 
+    $results = $this->edanRequestManager->getNmaahcFBList($q, $place, $params, $rows);
+
+    \Drupal::service('page_cache_kill_switch')->trigger();
     return [
       '#theme' => 'search-results',
       '#results' => $results,
       '#q' => str_replace("+", " ", $q),
       '#place' => $place,
       '#rows' => $rows,
+      '#params' => $params,
     ];
   }
 }
--- src/EDAN/EDANRequestManager.php
+++ src/EDAN/EDANRequestManager.php
@@ -8,31 +8,71 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class EDANRequestManager
 {
-  private $rows;
   protected $connector;
+  protected $fname_boost;
+  protected $lname_boost;
+  protected $location_boost;
+  protected $rtype_boost;
 
   public function __construct(EdanConnectorService $edanConnector)
   {
     $this->connector = $edanConnector;
 
     $fb_config = \Drupal::config('fb_search.settings');
-    $this->rows = $fb_config->get('display.rows');
+    $this->fname_boost = $fb_config->get('search.fname');
+    $this->lname_boost = $fb_config->get('search.lname');
+    $this->location_boost = $fb_config->get('search.location');
+    $this->rtype_boost = $fb_config->get('search.rtype');
   }
 
   public static function create(ContainerInterface $container) {
 		 return new static($container->get('edan.connector'));
 	}
 
-  public function getNmaahcFBList($q, $start=0)
+  protected function processParams($params)
   {
-    $startrows = $start * $this->rows;
+    $fqs = [];
+
+    array_push($fqs, "type:nmaahc_fb");
+
+    foreach($params as $key => $value)
+    {
+      switch($key)
+      {
+        case "fname":
+          array_push($fqs, "p.nmaahc_fb.pr_name_gn:$value^$this->fname_boost");
+          break;
+        case "lname":
+          array_push($fqs, "p.nmaahc_fb.pr_name_surn:$value^$this->lname_boost");
+          break;
+        case "location":
+          array_push($fqs, "p.nmaahc_fb.location:$value^$this->location_boost");
+          break;
+        case "rtype":
+          array_push($fqs, "p.nmaahc_fb.record_pub_number:$value^$this->rtype_boost");
+          break;
+        case "date":
+          array_push($fqs, "p.nmaahc_fb.index.search_date:[" . $value[0] . " TO " . $value[1] . "]");
+          break;
+      }
+    }
+
+    return json_encode($fqs);
+  }
+
+  public function getNmaahcFBList($q, $start=0, $fqs=[], $rows=10)
+  {
+    $fqs = $this->processParams($fqs);
+    $startrows = $start * $rows;
+
+    //\Drupal::logger('fb-search')->notice("$fqs");
 
     $params = [
-      "rows" => $this->rows,
+      "rows" => $rows,
       "start" => $startrows,
       "facets" => false,
       "q" => $q,
-      "fqs" => '["type:\"nmaahc_fb\""]'
+      "fqs" => $fqs
     ];
 
     $results = $this->connector->runParamQuery($params);
